d5ebdd1e503feefc42ef44c1302df34be8b098ad
Merzaad <88736874+Merzaad@users.noreply.github.com>
2021-12-15 11:55:44 +0330

    [0.30, 0.85]

--- HeartDisease/heartdisease.py
+++ HeartDisease/heartdisease.py
@@ -2,6 +2,7 @@ import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 encoder = LabelEncoder()
 
 data=pd.read_csv('heart.csv')
@@ -17,22 +18,25 @@ data['RestingECG_encoded'] = encoder.transform(data['RestingECG'])
 encoder.fit(data['ExerciseAngina'])
 data['ExerciseAngina_encoded'] = encoder.transform(data['ExerciseAngina'])
 
-
-x=data.drop(columns=['HeartDisease','ChestPainType','Sex','ST_Slope','RestingECG','ExerciseAngina'])
+x1=data.drop(columns=['HeartDisease','ChestPainType','Sex','ST_Slope','RestingECG','ExerciseAngina'])
+x=((x1-x1.min())/(x1.max()-x1.min()))*20
 y=data['HeartDisease']
-print(y.shape)
-
 x_train , x_test , y_train , y_test = train_test_split(x,y,test_size=0.2)
 
 model=tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.Dense(256, input_shape=(None, 763, 8), activation='sigmoid')) 
-model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(100, input_shape=(None,11), activation='sigmoid')) 
+model.add(tf.keras.layers.Dense(100, activation='sigmoid'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-
-
-model.fit(x_train,y_train, epochs=100 )
+heart_disease=model.fit(x_train,y_train, epochs=40)
+model.summary()
 model.evaluate(x_test,y_test)
+
+plt.plot(heart_disease.history['loss'], label='MAE (training data)')
+plt.plot(heart_disease.history['accuracy'], label='MAE (validation data)')
+plt.legend(['loss', 'accuracy'])
+plt.xlabel('epoch')
+plt.show()
